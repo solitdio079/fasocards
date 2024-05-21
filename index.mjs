e6aa971e67a65a88748e690ec744fcdc62f5fd29
solitdio079 <solitdio079@gmail.com>
2024-05-21 21:02:23 +0300

    removed localhost

--- index.mjs
+++ index.mjs
@@ -20,18 +20,12 @@ try {
 // Instantiating the express app
 const app = express()
 
-const corsOptions = [
-  {
-    origin: 'http://localhost:5173',
-    credentials: true,
-    optionsSuccessStatus: 200,
-  },
-  {
+const corsOptions = {
     origin: 'https://fasocard.com',
     credentials: true,
     optionsSuccessStatus: 200,
-  },
-]
+  }
+
 
 app.use(cors(corsOptions))
 app.use(cookieParser("secret"))
