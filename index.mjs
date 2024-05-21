a31696f7038a9b5460475174f1e98957619b9ab7
solitdio079 <solitdio079@gmail.com>
2024-05-21 20:47:45 +0300

    locked out

--- index.mjs
+++ index.mjs
@@ -20,13 +20,20 @@ try {
 // Instantiating the express app
 const app = express()
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true,
-  optionsSuccessStatus: 200,
-}
+const corsOptions = [
+  {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionsSuccessStatus: 200,
+  },
+  {
+    origin: 'https://fasocard.com',
+    credentials: true,
+    optionsSuccessStatus: 200,
+  },
+]
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(cookieParser("secret"))
 app.use(express.json())
 app.use(
