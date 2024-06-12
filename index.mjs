ad21ee11b8896a0769ea1103a8aa27b2384b4d55
solitdio079 <solitdio079@gmail.com>
2024-06-12 04:03:05 +0300

    uploading?

--- index.mjs
+++ index.mjs
@@ -23,7 +23,7 @@ try {
 // Instantiating the express app
 const __dirname = path.resolve()
 const app = express()
-app.use('/static', express.static(path.join(__dirname, '/uploads')))
+app.use('/static', express.static('/uploads'))
 
 const corsOptions = {
     origin: ['https://fasocard.com','http://localhost:5173'],
--- src/routes/business.mjs
+++ src/routes/business.mjs
@@ -13,7 +13,7 @@ const __dirname=path.resolve()
 // Business photos storage setup
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '/uploads/businesses'))
+    cb(null, '/uploads/businesses')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix =
